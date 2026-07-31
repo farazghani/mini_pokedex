c1cbf7d2c5cfa219165443008219a7e58249be65
Faraz Ghani <farazghani01@gmail.com>
2026-07-31 12:21:56 +0530

    chore(core): updated app.confiq added echarts and HTTPclient setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,30 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from "@angular/common/http";
+import { provideEchartsCore } from "ngx-echarts";
+import * as echarts from "echarts/core";
+import { LineChart } from "echarts/charts";
 import { routes } from './app.routes';
+
+import {GridComponent,TooltipComponent,LegendComponent}  from "echarts/components";
+import { CanvasRenderer } from "echarts/renderers";
+
+echarts.use([
+  LineChart,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  CanvasRenderer,
+  ]); 
+
+  
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideEchartsCore({ echarts }),
   ]
 };
+
