b2c24f883eb8e5d7c3b468f71c3de96cd3c58108
Faraz Ghani <farazghani01@gmail.com>
2026-07-31 18:54:25 +0530

    chore(core): finalised core

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    loadComponent: () =>
+      import('./pokedex/pages/pokedex-page/pokedex-page.component').then(
+        (m) => m.PokedexPageComponent
+      ),
+  },
+];
