2ce137f938b454bc6d8d57cab6ac527a76bf1d43
Faraz Ghani <farazghani01@gmail.com>
2026-08-01 02:47:42 +0530

    fix(core): fixed route bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,4 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-    path: '',
-    loadComponent: () =>
-      import('./pokedex/pages/pokedex-page/pokedex-page.component').then(
-        (m) => m.PokedexPageComponent
-      ),
-  },
 ];
