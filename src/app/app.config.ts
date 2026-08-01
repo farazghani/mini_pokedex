10cac64a7fcd4c39c6f19fe3aca8f7a21bcf0bd0
Faraz Ghani <farazghani01@gmail.com>
2026-08-01 14:09:48 +0530

    style(core): added global ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,37 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, inject } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+
+import { provideApollo } from 'apollo-angular';
+
 import { provideEchartsCore } from 'ngx-echarts';
 import * as echarts from 'echarts/core';
 import { LineChart } from 'echarts/charts';
-import { routes } from './app.routes';
-
-import { createApollo } from './core/graphql/apollo.config';
-import { GridComponent, TooltipComponent, LegendComponent } from 'echarts/components';
+import {
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+} from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
-import { provideApollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client';
 
-echarts.use([LineChart, GridComponent, TooltipComponent, LegendComponent, CanvasRenderer]);
+import { routes } from './app.routes';
+import { createApollo } from './core/graphql/apollo.config';
+
+echarts.use([
+  LineChart,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  CanvasRenderer,
+]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideEchartsCore({ echarts }),
-    provideHttpClient(),
     provideApollo(createApollo),
-    provideApollo(() => {
-      const httpLink = inject(HttpLink);
-
-      return {
-        link: httpLink.create({
-          uri: '<%= endpoint %>',
-        }),
-        cache: new InMemoryCache(),
-      };
-    }),
+    provideEchartsCore({ echarts }),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {
+    path: '',
+    loadComponent: () =>
+      import('./pokedex/pages/pokedex-page.component').then(
+        (m) => m.PokedexPageComponent,
+      ),
+  },
 ];
+
